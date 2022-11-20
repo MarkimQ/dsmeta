6301314fe3fb106b590db50c8cb8b1c46e6834e5
Marco Queiroz <marcoant.queiroz@gmail.com>
2022-11-20 17:59:41 -0300

    Toast

--- frontend/src/components/Notificationbutton/index.tsx
+++ frontend/src/components/Notificationbutton/index.tsx
@@ -1,10 +1,16 @@
+import { toast } from "react-toastify"
 import icon from "../../assets/img/notification-icon.svg"
 import "./styles.css"
 
+function handleClick(){
+    return(
+        toast.info("SMS enviado com sucesso")
+    )
+}
 
 function NotificationButton() {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick()}>
             <img src={icon} alt="Notificar" />
         </div>
     )
